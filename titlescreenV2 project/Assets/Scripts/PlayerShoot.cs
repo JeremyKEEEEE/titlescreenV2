c3c7e3617ab4b018090fc0092516ecac4c3e394f
JeremyKEEEEE <ke.jeremy@maranathastudents.org>
2023-05-15 12:19:14 -0700

    cannon

---
+++ titlescreenV2 project/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+   
+   [SerializeField]
+    private float _bulletSpeed;
+
+    [SerializeField]
+    private Transform _gunOffset;
+
+    [SerializeField]
+    private float _timeBetweenShots;
+    
+    private bool _fireContinuously;
+    private float _lastFireTime;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_fireContinuously)
+        {
+            float timeSinceLastFire = Time.time - _lastFireTime;
+            if (timeSinceLastFire >= _timeBetweenShots)
+            {
+                FireBullet();
+                _lastFireTime = Time.time;
+            }
+        }
+    }
+    private void FireBullet()
+    {
+        GameObject bullet = Instantiate(_bulletPrefab, _gunOffset.position, _gunOffset.rotation);
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+        rigidbody.velocity = _bulletSpeed*transform.up;
+    }
+
+    private void OnFire(InputValue inputValue)
+    {
+        _fireContinuously = inputValue.isPressed;
+    }
+
+
+}
